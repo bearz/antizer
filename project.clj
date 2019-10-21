3ab6fb272b4f18a254be1a1bc0aaa4d723b57bb6
Sergey Shvets <shvets.serg@gmail.com>
2019-10-20 17:42:15 -0700

    Updated antd dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
-                 [cljsjs/antd "3.19.8-0"]
+                 [cljsjs/antd "3.23.4-0"]
                  [cljsjs/moment "2.24.0-0"]]
   :plugins [[lein-codox "0.10.3"]]
   :codox {:language :clojurescript
